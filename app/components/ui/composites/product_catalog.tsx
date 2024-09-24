fba1f09f5a6b0a68c11c13f4260eb901f2f16234
Nathan Jones <naynayrjones@gmail.com>
2024-09-23 19:22:12 -0600

    made product-catalog client side

--- app/components/ui/composites/product_catalog.tsx
+++ app/components/ui/composites/product_catalog.tsx
@@ -1,19 +1,24 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import ProductCard from "@/app/components/ui/composites/product_card";
 import { ProductModel } from "@/app/components/model/product_model";
 
 async function getProducts() {
   const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
-  const response = await fetch(`${baseUrl}/api/product/catalog`, {
-    cache: 'force-cache' // This enables caching
-  });
+  const response = await fetch(`${baseUrl}/api/product/catalog`);
   if (!response.ok) {
     throw new Error(`HTTP error! status: ${response.status}`);
   }
   return response.json();
 }
 
-export default async function ProductCatalog() {
-  const products: ProductModel[] = await getProducts();
+export default function ProductCatalog() {
+  const [products, setProducts] = useState<ProductModel[]>([]);
+
+  useEffect(() => {
+    getProducts().then(setProducts);
+  }, []);
   
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
